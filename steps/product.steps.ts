228b809d5e4194cae24ad183443d707ca08c2119
tomnguyen2604 <tomnguyen2604@gmail.com>
2026-03-19 23:28:25 -0400

    Small updates

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,15 +1,21 @@
-import { Then } from '@cucumber/cucumber';
-import { getPage } from '../playwrightUtilities';
-import { Product } from '../pages/product.page';
+import { Then } from "@cucumber/cucumber";
+import { getPage } from "../playwrightUtilities";
+import { Product } from "../pages/product.page";
 
-Then('I will add the backpack to the cart', async () => {
-  await new Product(getPage()).addBackPackToCart();
+Then("I will add the backpack to the cart", async () => {
+    await new Product(getPage()).addBackPackToCart();
 });
 
-Then('I sort the products by {string}', async (sortOption) => {
-  await new Product(getPage()).sortProductsBy(sortOption);
+Then("I sort the products by {string}", async (sortOption) => {
+    await new Product(getPage()).sortProductsBy(sortOption);
 });
 
-Then('I should see {int} products sorted by price in {string} order', async (expectedCount, order) => {
-  await new Product(getPage()).validateProductPricesSorted(order, expectedCount);
-});
+Then(
+    "I should see {int} products sorted by price in {string} order",
+    async (expectedCount, order) => {
+        await new Product(getPage()).validateProductPricesSorted(
+            order,
+            expectedCount,
+        );
+    },
+);
